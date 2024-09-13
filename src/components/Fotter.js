2327024860e45d60fd23483c24fc3db1a8029e4b
nikitha7688 <nikitha7688@gmail.com>
2024-09-13 10:04:02 +0530

    update project

---
+++ src/components/Fotter.js
@@ -0,0 +1,24 @@
+//Fotter Component
+function Fotter()
+{
+  return(
+    <footer>
+    
+    <div className="contact">
+
+        
+        <h3>Contact</h3>
+        <p style={{marginTop: "5px;"}}>+91 7569883468<br></br>
+            perfumy2024@gmail.com
+        </p>
+
+    </div>
+   <p style={{fontSize: "small;", padding: "20px;" ,color:" grey;"}}>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam unde quia, nulla itaque dolorem at? Deleniti, exercitationem quaerat sunt esse dolores hic voluptate. Architecto minus tempora a fugiat consectetur veniam.
+   </p>
+</footer>
+  )
+}
+
+
+export default Fotter
---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+//Navbar Compounent//
+function Navbar()
+{
+  return(
+    <nav>
+      <h1>Perfumy</h1>
+      <ul>
+        <li>Home |</li>
+        <li>Product |</li>
+        <li>Adout  |</li>
+        <li>Contact </li>
+      </ul>
+    </nav>
+     
+  )
+}
+
+export default Navbar
